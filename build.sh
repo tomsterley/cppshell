6b74b002d8f03fc16a2b104ecb31aa9a4f284fd3
tomsterley <tommynaj@gmail.com>
2017-03-30 11:18:02 -0400

    Initial Win32 Version

--- build.sh
+++
@@ -1,5 +0,0 @@
-echo "Removing previous build..."
-rm -f main.app
-echo "Building..."
-g++ -o main.app main.cpp
-echo "Done!"
--- main.cpp
+++ main.cpp
@@ -6,26 +6,28 @@ using namespace std;
 string cmd;
 
 int main() {
-    
-    while(true) {
-        cout << ">: ";
-        getline(cin, cmd);
-        
-        if(cmd == "version") {
-            cout << "Tomsterley Shell v1.2" << endl;
-        }
-        else {
-            if(cmd == "exit") {
-                return 0;
-            }
-            else {
-                if(cmd == "help") {
-                    cout << "Commands: version, exit, help" << endl;
-                }
-                else {
-                    cout << "Error: '" << cmd << "' is not a valid command" << endl;
-                }
-            }
-        }
-    }
+	while (true) {
+		cout << ">: ";
+		getline(cin, cmd);
+
+		if (cmd == "version") {
+			cout << "Tomsterley Shell v1.2-Win32" << endl;
+		}
+
+		else {
+			if (cmd == "exit") {
+				return 0;
+			}
+
+			else {
+				if (cmd == "help") {
+					cout << "Commands: version, exit, help" << endl;
+				}
+
+				else {
+					cout << "Error: '" << cmd << "' is not a valid command" << endl;
+				}
+			}
+		}
+	}
 }
